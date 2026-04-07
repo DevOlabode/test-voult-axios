ae0e35e4c7e33d5385d2613517979bef9c56ff5a
Samuel Olabode <solabode499@gmail.com>
2026-04-07 05:35:05 -0600

    add session configuration for express-session: define session settings including secret, cookie options, and expiration for improved session management

---
+++ config/session.js
@@ -0,0 +1,14 @@
+const sessionConfig = {
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+       secure : false,
+        httpOnly: true,
+        sameSite: 'lax',
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    }
+};
+
+module.exports = sessionConfig;
